6b4de2f1fa58d6681e5cc3fa3eb2c624bb74de90
Alexander Schäfer <alex.schaefer.12.05@gmail.com>
2024-10-10 23:40:26 +0200

    Implement argument parsing and validation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,69 @@
 use std::env;
+use std::net::IpAddr;
+use std::process;
+use std::str::FromStr;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        } else if args.len() > 4 {
+            return Err("too many arguments");
+        }
+
+        let f = args[1].clone();
+        match IpAddr::from_str(&f) {
+            Ok(ipaddr) => {
+                return Ok(Arguments {
+                    flag: String::from(""),
+                    ipaddr,
+                    threads: 4,
+                });
+            }
+            Err(_) => {
+                let flag = args[1].clone();
+                if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
+                    println!("Usage: -j to select how many threads you want -h or -help to show this help message");
+                    return Err("help");
+                } else if flag.contains("-h") || flag.contains("-help") {
+                    return Err("to many arguments");
+                } else if flag.contains("-j") && args.len() == 3 {
+                    let ipaddr = match IpAddr::from_str(&args[3]) {
+                        Ok(s) => s,
+                        Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
+                    };
+                    let threads = match args[2].parse::<u16>() {
+                        Ok(s) => s,
+                        Err(_) => return Err("failed to parse thread number"),
+                    };
+                    return Ok(Arguments {
+                        threads,
+                        flag,
+                        ipaddr,
+                    });
+                } else {
+                    return Err("invalid syntax");
+                }
+            }
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    for i in &args {
-        println!("arg = {}", i)
-    }
+    let program = args[0].clone();
+    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
+        if err.contains("help") {
+            process::exit(0);
+        } else {
+            eprintln!("{} problem parsing arguments: {}", program, err);
+            process::exit(0);
+        }
+    });
 }
